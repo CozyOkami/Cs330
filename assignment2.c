a7341749948f7f06960e1c5501add63467b42267
CozyOkami <143756067+CozyOkami@users.noreply.github.com>
2023-09-08 16:00:18 +0000

    finished problem 2

--- assignment2.c
+++ assignment2.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 void cubeOfodd (int n){
-    for(int i = 1; i < n; i + 2 ){
+    for(int i = 1; i < n; i += 2 ){
         printf(i*i*i);
     }
 }
 
+void introToCS330 (int n){
+    if (n % 7 == 0 && n % 3 == 0){
+        printf("UAB CS 330");
+    } if (n % 7 == 0){
+        printf("UAB");
+    } if (n % 3 == 0){
+        printf("CS");
+    } if (n > 1 && n != 3 && n != 7){
+        for (int i = 2; i < n; i += 1) {
+            if (n % i == 0){
+                printf("Go Blazers");
+            }
+        }
+    } else{
+        printf(n*n*n);
+    }
+}
