b91426efc7a800d82f8a86baf68b5331879378db
CozyOkami <143756067+CozyOkami@users.noreply.github.com>
2023-09-11 00:56:19 +0000

    sadly removed user input

--- hw2/asgn2.c
+++ hw2/asgn2.c
@@ -11,42 +11,5 @@ int main(){
     int avg_hw;
     int attendance;
 
-    printf("which function would you like to run?");
-    printf("1 for cubeOfodd ");
-    printf("2 for introToCS330");
-    printf("3 for printHello");
-    printf("4 for paintGallons");
-    printf("5 for grader");
-    scanf("%d", &func);
-    if(scanf("%d", &func) == 1){
-        printf("please enter 'n' for problem 1");
-        scanf("%d", &n);
-        cubeOfodd(n);
-    }if(scanf("%d", &func) == 2){
-        printf("please enter 'n' for problem 2");
-        scanf("%d", &n);
-        introToCS330(n);
-    }if(scanf("%d", &func) == 3){
-        printf("please enter 'n' for problem 3");
-        scanf("%d", &n);
-        printHello(n);
-    }if(scanf("%d", &func) == 4){
-        printf("please enter length");
-        scanf("%d", &length);
-        printf("please enter width");
-        scanf("%d", &width);
-        printf("please enter height");
-        scanf("%d", &height);
-        paintGallons(length, width, height);
-    } if(scanf("%d", &func) == 5){
-        printf("please enter exam average");
-        scanf("%d", &avg_exams);
-        printf("please enter homework average");
-        scanf("%d", &avg_hw);
-        printf("please enter attandance");
-        scanf("%d", &attendance);
-        grader(avg_exams, avg_hw, attendance);
-    }else{
-        printf("Sorry I didn't get that");
-    } return 0;
+    return 0;
 }
