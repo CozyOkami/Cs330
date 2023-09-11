438ee22e93ddf1c8de2d8fc6629bbdfb1e287bc0
CozyOkami <143756067+CozyOkami@users.noreply.github.com>
2023-09-11 00:58:27 +0000

    called functions in main

--- hw2/asgn2.c
+++ hw2/asgn2.c
@@ -10,6 +10,10 @@ int main(){
     int avg_exams;
     int avg_hw;
     int attendance;
-
+    cubeOfodd(n);
+    introToCS330(n);
+    printHello(n);
+    paintGallons(length,width,height);
+    grader(avg_exams,avg_hw,attendance);
     return 0;
 }
